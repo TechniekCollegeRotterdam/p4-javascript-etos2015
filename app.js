e3f7312f7325c4192991e7f30ae36acece5291a1
etos2015 <9016506@student.zadkine.nl>
2022-05-16 15:00:16 +0200

    dom2

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ let name;
 let country;
 let p = document.getElementById("output");
 
-const nameChanged = function(event) {
-    name = event.target.value;
-    changeInput();
-}
 
-const countryChanged = function(event){
+let nameChanged = document.querySelector("#nameChanged");
+let countryChanged = document.querySelector("#countryChanged");
+
+nameChanged.addEventListener("change", function(){
+ name = event.target.value;
+ changeInput();
+})
+
+countryChanged.addEventListener("change", function(){
     country = event.target.value;
     changeInput();
-}
+   })
 
 const changeInput = function(){
     const text = `Je heet ${name} en je komt uit ${country}`; 
