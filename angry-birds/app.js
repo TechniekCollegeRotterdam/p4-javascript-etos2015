da097970bbbdf53c3454aebb2556e4949aafde71
etos2015 <9016506@student.zadkine.nl>
2022-06-03 11:11:19 +0200

    bir

--- angry-birds/app.js
+++ angry-birds/app.js
@@ -15,21 +15,25 @@ window.addEventListener("keydown", function(e){
     if(e.key == "ArrowRight"){    
         count += 50;
         birds.style.left = count + "px";
+        birds.src= "img/b-right.svg";
     }
 
 
 if(e.key == "ArrowLeft"){    
     count -= 50;
     birds.style.left = count + "px";
+    birds.src = "img/b-left.svg";
 }
 
 if(e.key == "ArrowUp"){    
     count += 50;
-    birds.style.bottom = count + "px";
+    birds.style.bottom = count + "px"
+    birds.src= "img/b-up.svg";
 }
 
 if(e.key == "ArrowDown"){    
     count -= 50;
     birds.style.bottom = count + "px";
+    birds.src= "img/b-down.svg";
 }
 });
